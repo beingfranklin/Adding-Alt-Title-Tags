450593ea5f5ab7ac8662c49cb61c93a1b16d8098
tune2tech <franklinanton.007@gmail.com>
2014-04-07 09:57:41 +0530

    Create alt-tags.js

---
+++ alt-tags.js
@@ -0,0 +1,1 @@
+$undefineddocument).readyundefinedfunctionundefined) { $undefined'img').eachundefinedfunctionundefined){ var $img = $undefinedthis); var filename = $img.attrundefined'src') $img.attrundefined'alt', filename.substringundefinedundefinedfilename.lastIndexOfundefined'/'))+1, filename.lastIndexOfundefined'.'))); }); });
