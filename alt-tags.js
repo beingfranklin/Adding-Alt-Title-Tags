d9e2da9fdf3d981faa7de32ec838aa223d5c9aa4
Franklin Antony <franklinanton.007@gmail.com>
2017-11-06 01:25:30 +0530

    Update alt-tags.js

--- alt-tags.js
+++ alt-tags.js
@@ -1,1 +1,12 @@
-$undefineddocument).readyundefinedfunctionundefined) { $undefined'img').eachundefinedfunctionundefined){ var $img = $undefinedthis); var filename = $img.attrundefined'src') $img.attrundefined'alt', filename.substringundefinedundefinedfilename.lastIndexOfundefined'/'))+1, filename.lastIndexOfundefined'.'))); }); });
+//Adding Alt and Title Tags 
+//The Names are taken from the FileName. So Name the images accordingly.
+
+$undefineddocument).readyundefinedfunctionundefined) {
+    $undefined 'img').eachundefinedfunctionundefined) {
+      // Here the img tags are being altered.
+    var $img = $undefinedthis);
+   
+var filename = $img.attrundefined 'src') $img.attrundefined 'alt', filename.substringundefinedundefinedfilename.lastIndexOfundefined '/')) + 1, filename.lastIndexOfundefined '.')));
+
+    });
+});
